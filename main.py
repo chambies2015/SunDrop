0d6c163f57a785407c77189f72c5d4cdb7f0f2cb
dan <xeonchambies@gmail.com>
2023-02-17 15:18:12 -0600

    added automated cat pics.

--- main.py
+++ main.py
@@ -26,6 +26,23 @@ client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 
 @client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name} (ID: {client.user.id})')
+    # Set a task to send a random cat picture every 24 hours
+    await asyncio.create_task(send_cat_picture())
+
+
+async def send_cat_picture():
+    channel = client.get_channel(1076251107619246182) # Replace CHANNEL_ID with the actual ID of the channel you want to send the pictures to
+    while True:
+        # Get a random cat picture from the Cat API
+        response = requests.get('https://api.thecatapi.com/v1/images/search')
+        if response.status_code == 200:
+            # Send the picture to the channel
+            await channel.send(response.json()[0]['url'])
+        # Wait for 24 hours before sending the next picture
+        await asyncio.sleep(24 * 60)
+@client.event
 async def on_message(message):
     if message.content.startswith("!weather"):
 
